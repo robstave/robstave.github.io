bfe80c1cb22ebaf8eb0b319d32bd2af403e52150
Rob Stave <robstave@yahoo.com>
2015-11-27 13:18:34 -0600

    Webpage
    phase1

---
+++ acs/script.js
@@ -0,0 +1,14 @@
+// Code goes here
+
+var app = angular.module('main', []);
+app.controller('MyController', function($scope) {
+
+     $scope.count = 1000;
+	 $scope.helloWorld = "ssss";
+	  
+});
+
+ 
+
+ 
+ 
